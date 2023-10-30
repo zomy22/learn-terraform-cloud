8a5e36a6bf16b9def4a2fa0b4c77a2572fa9a0a3
zomy22 <uzoma_c_ogbonna@yahoo.com>
2023-10-30 18:53:08 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,6 @@
 terraform {
 
+/*
   cloud {
     organization = "organization-name"
 
@@ -7,11 +8,12 @@ terraform {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 5.23.0"
     }
   }
 
